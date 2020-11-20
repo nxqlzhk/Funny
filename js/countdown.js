541ed737b723efec229157b801e2db4852a941f5
nxqlzhk <seven24678@163.com>
2020-11-20 22:54:13 +0800

    轮播图等

---
+++ js/countdown.js
@@ -0,0 +1,34 @@
+// 求两个日期之间差
+function getInterval(start, end) {
+    // 两个日期对象，相差的毫秒数
+    let interval = end - start;
+    // 求 相差的天数/小时数/分钟数/秒数
+    let day, hour, minute, second;
+
+    // 两个日期对象，相差的秒数
+    // interval = interval / 1000;
+    interval /= 1000;
+
+    day = Math.round(interval / 60 / 60 / 24);
+    hour = Math.round(interval / 60 / 60 % 24);
+    minute = Math.round(interval / 60 % 60);
+    second = Math.round(interval % 60);
+
+    return {
+        day: day,
+        hour: hour,
+        minute: minute,
+        second: second
+    };
+}
+
+// 处理innerText和textContent的兼容性问题
+// 设置标签之间的内容
+function setInnerText(element, content) {
+    // 判断当前浏览器是否支持 innerText
+    if (typeof element.innerText === 'string') {
+        element.innerText = content;
+    } else {
+        element.textContent = content;
+    }
+}
